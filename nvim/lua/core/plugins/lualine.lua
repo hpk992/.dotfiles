335d02085b80999d8ea2449b6ee5999f1140b110
hpk992 <83901776+hpk992@users.noreply.github.com>
2024-05-28 01:20:48 -0700

    lualine adjs

--- nvim/lua/core/plugins/lualine.lua
+++ nvim/lua/core/plugins/lualine.lua
@@ -25,7 +25,7 @@ return {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { { "filename", path = 1 } },
-				lualine_x = { "fileformat", "filetype" },
+				lualine_x = { "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
